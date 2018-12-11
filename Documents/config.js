e0402e7391c95c3130295f8c3535f54282b5ad0c
priyal97 <priyalv@bu.edu>
2018-12-11 18:46:03 -0500

    Credentials for Google authorization

---
+++ Documents/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '870388069437-o6tdkgbtt2dho4tncb8r2r969nm95eno.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '7NPYC1ZV5LoBR207zQb7UvL4',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
